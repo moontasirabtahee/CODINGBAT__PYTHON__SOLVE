9fc15c9e2e3933b7a5b492f258d3dfd710f8661c
M.A.Moontasir Abtahee <mamoontasir.abtahee@gmail.com>
2021-05-29 16:13:46 +0600

    Logic-1

--- 5. Logic-1/sorta_sum.py
+++ 5. Logic-1/sorta_sum.py
@@ -1,1 +1,12 @@
 # Created by Moontasir Abtahee at 5/29/2021
+# Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+# sorta_sum(3, 4) → 7
+# sorta_sum(9, 4) → 20
+# sorta_sum(10, 11) → 21
+
+def sorta_sum(a, b):
+    total = (a+b)
+    if total in range(10,20,1):
+        return 20
+    else:
+        return total
