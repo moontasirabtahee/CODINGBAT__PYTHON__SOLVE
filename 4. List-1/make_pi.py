6edff2138bcc29110755f094db0a7d885042898a
M.A.Moontasir Abtahee <mamoontasir.abtahee@gmail.com>
2021-05-29 14:57:17 +0600

    List-1

---
+++ 4. List-1/make_pi.py
@@ -0,0 +1,5 @@
+# Created by Moontasir Abtahee at 5/29/2021
+# Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+# make_pi() → [3, 1, 4]
+def make_pi():
+    return [3,1,4]
